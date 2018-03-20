1ae52f3fe30bf2cc7ef8bc56ddff1596f0577b6c
jntakpe <jntakpe@gmail.com>
2018-03-20 21:20:25 +0100

    Update

--- src/main/kotlin/com/github/jntakpe/sleuthmongodb/User.kt
+++ src/main/kotlin/com/github/jntakpe/sleuthmongodb/User.kt
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.io.Serializable
 
+//If you remove the indexed annotation it will work fine
 @Document
 data class User(@Indexed val username: String, val id: ObjectId? = null): Serializable
